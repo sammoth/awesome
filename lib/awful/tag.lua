9821b0e5ff6221fc51f0bdbe7ef8831835e559bb
Emmanuel Lepage Vallee <elv1313@gmail.com>
2016-04-11 04:25:31 -0400

    doc: Improve awful.tag and awful.screen with images

--- lib/awful/tag.lua
+++ lib/awful/tag.lua
@@ -658,6 +658,14 @@ end
 -- See the layout suit documentation for information about how the master width
 -- factor is used.
 --
+-- Here is the property effect on the tiled layouts:
+--
+--@DOC_awful_layout_dynamic_suit_tile_mwfact_EXAMPLE@
+--
+-- Here is the property effect on the magnifier layout:
+--
+--@DOC_awful_layout_dynamic_suit_magnifier_mwfact_EXAMPLE@
+--
 -- **Signal:**
 --
 -- * *property::mwfact* (deprecated)
@@ -940,6 +948,8 @@ end
 -- This property allow to waste space on the screen in the name of style,
 -- unicorns and readability.
 --
+-- @DOC_awful_layout_dynamic_suit_tile_gap_EXAMPLE@
+--
 -- **Signal:**
 --
 -- * *property::useless_gap*
@@ -1116,6 +1126,10 @@ end
 
 --- Set the number of master windows.
 --
+-- Here is the property effect on the tiled layouts:
+--
+--@DOC_awful_layout_dynamic_suit_tile_nmaster_EXAMPLE@
+--
 -- **Signal:**
 --
 -- * *property::nmaster* (deprecated)
@@ -1229,6 +1243,10 @@ end
 -- @see column_count
 
 --- Set the number of columns.
+--
+-- Here is the property effect on the tiled layouts:
+--
+--@DOC_awful_layout_dynamic_suit_tile_ncol_EXAMPLE@
 --
 -- **Signal:**
 --
