5bc1a915edae81c179aacd7bf2c16e354df2abf4
Emmanuel Lepage Vallee <elv1313@gmail.com>
2019-05-25 09:49:13 -0400

    rc.lua: Invert how rules are set.
    Rather than directly set the rules in rc.lua, listen to the awful.rules
    module. Once the module requests rules, set them.
    
    It helps on 2 fronts. First, it is in line with the goal of doing blocks
    that can be cut+pasted into other files if the user wish so. It also
    help modules listen to the signals sent by gears.matcher and modify (or
    block) individual rules. This will require more commits before it is
    "really" useful compared to just setting the rules directly.
    
    It is also important to keep in mind that splitting `gears.matcher` into
    a module was done so it can be used for notifications, clipboard and
    tags too. Those modules will provide a similar API. It is probably more
    useful there than it is for the client.

--- awesomerc.lua
+++ awesomerc.lua
@@ -442,58 +442,60 @@ root.keys(globalkeys)
 -- Rules to apply to new clients (through the "manage" signal).
 -- @DOC_RULES@
 
--- @DOC_GLOBAL_RULE@
--- All clients will match this rule.
-awful.rules.add_rule {
-    rule       = { },
-    properties = {
-        border_width = beautiful.border_width,
-        border_color = beautiful.border_normal,
-        focus        = awful.client.focus.filter,
-        raise        = true,
-        keys         = clientkeys,
-        buttons      = clientbuttons,
-        screen       = awful.screen.preferred,
-        placement    = awful.placement.no_overlap+awful.placement.no_offscreen
-    }
-}
-
--- @DOC_FLOATING_RULE@
--- Floating clients.
-awful.rules.add_rule {
-    rule_any = {
-        instance = { "copyq", "pinentry" },
-        class = {
-            "Arandr", "Blueman-manager", "Gpick", "Kruler", "Sxiv",
-            "Tor Browser", "Wpa_gui", "veromix", "xtightvncviewer"
-        },
-        -- Note that the name property shown in xprop might be set slightly after creation of the client
-        -- and the name shown there might not match defined rules here.
-        name = {
-            "Event Tester",  -- xev.
-        },
-        role = {
-            "AlarmWindow",    -- Thunderbird's calendar.
-            "ConfigManager",  -- Thunderbird's about:config.
-            "pop-up",         -- e.g. Google Chrome's (detached) Developer Tools.
+client.connect_signal("request::rules", function()
+    -- @DOC_GLOBAL_RULE@
+    -- All clients will match this rule.
+    awful.rules.append_rule {
+        rule       = { },
+        properties = {
+            border_width = beautiful.border_width,
+            border_color = beautiful.border_normal,
+            focus        = awful.client.focus.filter,
+            raise        = true,
+            keys         = clientkeys,
+            buttons      = clientbuttons,
+            screen       = awful.screen.preferred,
+            placement    = awful.placement.no_overlap+awful.placement.no_offscreen
         }
-    },
-    properties = { floating = true }
-}
+    }
 
--- @DOC_DIALOG_RULE@
--- Add titlebars to normal clients and dialogs
-awful.rules.add_rule {
-    -- @DOC_CSD_TITLEBARS@
-    rule_any   = { type = { "normal", "dialog" } },
-    properties = { titlebars_enabled = true      }
-}
+    -- @DOC_FLOATING_RULE@
+    -- Floating clients.
+    awful.rules.append_rule {
+        rule_any = {
+            instance = { "copyq", "pinentry" },
+            class = {
+                "Arandr", "Blueman-manager", "Gpick", "Kruler", "Sxiv",
+                "Tor Browser", "Wpa_gui", "veromix", "xtightvncviewer"
+            },
+            -- Note that the name property shown in xprop might be set slightly after creation of the client
+            -- and the name shown there might not match defined rules here.
+            name = {
+                "Event Tester",  -- xev.
+            },
+            role = {
+                "AlarmWindow",    -- Thunderbird's calendar.
+                "ConfigManager",  -- Thunderbird's about:config.
+                "pop-up",         -- e.g. Google Chrome's (detached) Developer Tools.
+            }
+        },
+        properties = { floating = true }
+    }
 
--- Set Firefox to always map on the tag named "2" on screen 1.
--- awful.rules.add_rules({
---     rule       = { class = "Firefox"     },
---     properties = { screen = 1, tag = "2" }
--- })
+    -- @DOC_DIALOG_RULE@
+    -- Add titlebars to normal clients and dialogs
+    awful.rules.append_rule {
+        -- @DOC_CSD_TITLEBARS@
+        rule_any   = { type = { "normal", "dialog" } },
+        properties = { titlebars_enabled = true      }
+    }
+
+    -- Set Firefox to always map on the tag named "2" on screen 1.
+    -- awful.rules.append_rule {
+    --     rule       = { class = "Firefox"     },
+    --     properties = { screen = 1, tag = "2" }
+    -- }
+end)
 
 -- }}}
 
