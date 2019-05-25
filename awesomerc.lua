79116447da62cfb678416d58c75ef1225d676b01
Emmanuel Lepage Vallee <elv1313@gmail.com>
2019-05-25 09:32:51 -0400

    rc.lua: Add (awful.)rules one by one.
    This is a step toward modularity. The next step is for awful.rules to
    request rules instead of rc.lua just setting them. The step after that
    is adding an identifier to the rules so modules can listen to rules
    being added and modify them as needed.

--- awesomerc.lua
+++ awesomerc.lua
@@ -441,64 +441,60 @@ root.keys(globalkeys)
 -- {{{ Rules
 -- Rules to apply to new clients (through the "manage" signal).
 -- @DOC_RULES@
-awful.rules.rules = {
-    -- @DOC_GLOBAL_RULE@
-    -- All clients will match this rule.
-    { rule = { },
-      properties = { border_width = beautiful.border_width,
-                     border_color = beautiful.border_normal,
-                     focus = awful.client.focus.filter,
-                     raise = true,
-                     keys = clientkeys,
-                     buttons = clientbuttons,
-                     screen = awful.screen.preferred,
-                     placement = awful.placement.no_overlap+awful.placement.no_offscreen
-     }
-    },
 
-    -- @DOC_FLOATING_RULE@
-    -- Floating clients.
-    { rule_any = {
-        instance = {
-          "DTA",  -- Firefox addon DownThemAll.
-          "copyq",  -- Includes session name in class.
-          "pinentry",
-        },
+-- @DOC_GLOBAL_RULE@
+-- All clients will match this rule.
+awful.rules.add_rule {
+    rule       = { },
+    properties = {
+        border_width = beautiful.border_width,
+        border_color = beautiful.border_normal,
+        focus        = awful.client.focus.filter,
+        raise        = true,
+        keys         = clientkeys,
+        buttons      = clientbuttons,
+        screen       = awful.screen.preferred,
+        placement    = awful.placement.no_overlap+awful.placement.no_offscreen
+    }
+}
+
+-- @DOC_FLOATING_RULE@
+-- Floating clients.
+awful.rules.add_rule {
+    rule_any = {
+        instance = { "copyq", "pinentry" },
         class = {
-          "Arandr",
-          "Blueman-manager",
-          "Gpick",
-          "Kruler",
-          "MessageWin",  -- kalarm.
-          "Sxiv",
-          "Tor Browser", -- Needs a fixed window size to avoid fingerprinting by screen size.
-          "Wpa_gui",
-          "veromix",
-          "xtightvncviewer"},
-
+            "Arandr", "Blueman-manager", "Gpick", "Kruler", "Sxiv",
+            "Tor Browser", "Wpa_gui", "veromix", "xtightvncviewer"
+        },
         -- Note that the name property shown in xprop might be set slightly after creation of the client
         -- and the name shown there might not match defined rules here.
         name = {
-          "Event Tester",  -- xev.
+            "Event Tester",  -- xev.
         },
         role = {
-          "AlarmWindow",  -- Thunderbird's calendar.
-          "ConfigManager",  -- Thunderbird's about:config.
-          "pop-up",       -- e.g. Google Chrome's (detached) Developer Tools.
+            "AlarmWindow",    -- Thunderbird's calendar.
+            "ConfigManager",  -- Thunderbird's about:config.
+            "pop-up",         -- e.g. Google Chrome's (detached) Developer Tools.
         }
-      }, properties = { floating = true }},
-
-    -- @DOC_DIALOG_RULE@
-    -- Add titlebars to normal clients and dialogs
-    { rule_any = {type = { "normal", "dialog" }
-      -- @DOC_CSD_TITLEBARS@
-      }, properties = { titlebars_enabled = true }
     },
-
-    -- Set Firefox to always map on the tag named "2" on screen 1.
-    -- { rule = { class = "Firefox" },
-    --   properties = { screen = 1, tag = "2" } },
+    properties = { floating = true }
 }
+
+-- @DOC_DIALOG_RULE@
+-- Add titlebars to normal clients and dialogs
+awful.rules.add_rule {
+    -- @DOC_CSD_TITLEBARS@
+    rule_any   = { type = { "normal", "dialog" } },
+    properties = { titlebars_enabled = true      }
+}
+
+-- Set Firefox to always map on the tag named "2" on screen 1.
+-- awful.rules.add_rules({
+--     rule       = { class = "Firefox"     },
+--     properties = { screen = 1, tag = "2" }
+-- })
+
 -- }}}
 
 -- {{{ Signals
